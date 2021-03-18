95aa8953bcbc4f8af8d4504d657f4c6f3f5a62b9
Shivam Kumar Roy <79640053+Shivam270997@users.noreply.github.com>
2021-03-18 17:31:45 +0530

    Update diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -28,63 +28,119 @@ void tokenize(string const &str, const char delim,vector<string> &out)
 
 int main()
 {
+        string A,B;
         string s1,s2;
         ifstream File1;
         ifstream File2;
-        ofstream f;
+        ofstream f1,f2,f;
         int ln=0;
         int count =0;
         string s;
         const char delim = ',';
+        cout<<"Enter the file 1 name"<<endl;
+        getline(cin,A);
+        cout<<"Enter the file 2 name"<<endl;
+        getline(cin,B);
 
-        File1.open("sample.txt");
-        File2.open("sample.txt");
-        while(getline(File1,s1)&&getline(File2,s2))
+        File1.open(A.c_str());
+        File2.open(B.c_str());
+        f.open("diff.txt");
+        while(1)
         {
+                getline(File1,s1);
+                getline(File2,s2);
+                if(File1.eof() && File2.eof()){
+                        break;}
+                if(File1.eof())
+                {
+                        f2.open("file2.txt");
+                        f2<<"Remaining line of File 2 is given below"<<endl;
+                        do
+                        {
+                                vector<string>e2;
+                                tokenize(s2,delim,e2);
+                                if(e2.size()==7)
+                                {
+                                        for(int k=0;k<7;k++){
 
+                                                f2<<e2[k]<<"\t";}
+                                }
+
+                                f2<<endl;
+                                e2.empty();
+                        }while(getline(File2,s2));
+                        f2.close();
+                        break;
+                }
+               if(File2.eof())
+                {
+                        f1.open("file1.txt");
+                        f1<<"Remaining lines of file 1 is given below"<<endl;
+
+                        do
+                        {
+                                vector<string>e1;
+                                tokenize(s1,delim,e1);
+                                if(e1.size()==7)
+                                {
+                                        for(int k=0;k<7;k++){
+
+                                                f1<<e1[k]<<"\t";}
+                                }
+                                f1<<endl;
+
+                                e1.empty();
+                        }while(getline(File1,s1));
+                        f1.close();
+                        break;
+                }
                 int i=0,j=0;
                 vector<string> out1;
                 vector<string> out2;
                 tokenize(s1,delim,out1);
                 tokenize(s2,delim,out2);
 
-        if(out1.size()<7 || out2.size()<7)
-        {
-                out1.empty();
-                out2.empty();
-                continue;
-        }
-        ln++;
-        stringstream geek1(out1[0]);
-        stringstream geek2(out2[0]);
-        int x1=0,x2=0;
-        geek1>>x1;
+                if(out1.size()<7 || out2.size()<7)
+                {
+                        out1.empty();
+                        out2.empty();
+                        continue;
+                }
+                ln++;
+                stringstream geek1(out1[0]);
+                stringstream geek2(out2[0]);
+                int x1=0,x2=0;
+                geek1>>x1;
                 geek2>>x2;
                 if(x1!=ln)
                         {
-                                cout<<"sequence is missing in file 1"<<endl;
+                                f<<"sequence is missing in file 1"<<endl;
+
                                 break;
                         }
                 if(x2!=ln)
                         {
-                                cout<<"sequence is missing in file 2";
+                                f<<"sequence is missing from file 2"<<endl;
+
                                 break;
                         }
                 while(i<7)
                         {
                                 if((out1[1]!="N"&&out1[1]!="M"&&out1[1]!="X"&&out1[1]!="T"))
                                         {
-                                        cout<<"from file 1 line no:- "<<ln<<" no N,M,X,T found"<<endl;
+                                        f<<"from file 1 line no:- "<<ln<<" no N,M,X,T found"<<endl;
+
                                         break;
                                         }
                                 if((out2[1]!="N"&&out2[1]!="M"&&out2[1]!="X"&&out2[1]!="T"))
                                         {
-                                        cout<<"from file 2 line no:- "<<ln<<" no N,M,X,T found"<<endl;
+                                        f<<"from file 2 line no:- "<<ln<<" no N,M,X,T found"<<endl;
+
                                         break;
                                         }
                                 if(out1[i].compare(out2[i])!=0)
                                         {
-                                        cout<<"in line "<<ln<< " has unmatched char"<<endl;
+                                        f<<"in both file line no:- "<<ln<<" has unmatched char data"<<endl;
 
                                         }
                                         i++;
@@ -94,5 +150,6 @@ int main()
         }
         f<<endl<<"END OF FILE"<<endl;
         cout<<"PROGRAM ENDS"<<endl;
+        f.close();
         return 0;
 }
